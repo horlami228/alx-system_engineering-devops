55bf42773c32f2ced5bf7e12f2e72af73d3bb868
Olamilekan <akintolaolamilekan51@gmail.com>
2023-11-09 15:51:07 +0100

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""This script queries the Reddit api and returns
+the title of the first 10 hot post for a subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the titles of the top ten posts in a given subreddit."""
+    if type(subreddit) is not str:
+        return 0
+    # Set up the request parameters
+    url = "https://www.reddit.com/r/{}/hot/.json?".format(subreddit)
+    params = {
+        'limit': 10,
+    }
+    set_header = {"User-Agent": 'Google Chrome Version 81.0.4044.129'}
+
+    res = requests.get(url, headers=set_header, params=params)
+    try:
+        data = res.json()["data"]["children"]
+        for title in data:
+            print(title["data"]["title"])
+    except Exception:
+        return 0
