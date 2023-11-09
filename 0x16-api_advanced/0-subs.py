0b72a400a11b6591e3e20ff53cdcf4aa2c18bba0
Olamilekan <akintolaolamilekan51@gmail.com>
2023-11-09 15:02:41 +0100

    subreddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""This script queries the Reddit API and returns
+the number of total subcribers"""
+from sys import argv
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit."""
+    if type(subreddit) is not str:
+        return 0
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+
+    response = requests.get(url, headers=user_agent)
+    try:
+        print(response.json()["data"]["subscribers"])
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+# Advanced API with Reddit
